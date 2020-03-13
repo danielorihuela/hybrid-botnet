c9d924f984aab510eda28bafc4da4c18ee05e5c3
danielorihuelarodriguez@gmail.com <danielorihuelarodriguez@gmail.com>
2020-03-13 19:14:01 +0100

    feature: change ed25519 for RSA

--- botnet_p2p/security.py
+++ botnet_p2p/security.py
@@ -1,74 +1,72 @@
 """Utilities to sign and verify messages"""
 
 import hashlib
-import cryptography.hazmat.backends.openssl.ed25519
-from cryptography.hazmat.primitives.asymmetric import ed25519
+import cryptography.hazmat.backends.openssl
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 from botnet_p2p import ENCODING
 
 
-def load_public_key(
-    public_key: str
-) -> cryptography.hazmat.backends.openssl.ed25519._Ed25519PublicKey:
-    """ Load the public Ed25519 key from the binary file
-
-        Args:
-            publick_key: Path where the public key is located
-
-        Returns:
-            The key in a format that the cryptography module
-            can manipulate
-    """
-    with open(public_key, "rb") as pub:
-        pub_bytes = pub.read()
-
-    return ed25519.Ed25519PublicKey.from_public_bytes(pub_bytes)
-
-
-def sign_hash(hash_: bytes, private_key: str) -> bytes:
-    """ Sign hash with a private key
-
-        Args:
-            hash_: Hash to sign
-            private_key: Path where private key is located
-
-        Returns:
-            The signed hash in bytes
-    """
-    with open(private_key, "rb") as f:
-        priv_bytes = f.read()
-    final_private_key = ed25519.Ed25519PrivateKey.from_private_bytes(priv_bytes)
-    return final_private_key.sign(hash_)
+def sign_hash(hash_: bytes, private_key_path: str) -> bytes:
+    private_key = load_private_key(private_key_path)
+    signed_hash = private_key.sign(
+        hash_,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signed_hash
 
 
 def calculate_hash(data: str) -> bytes:
-    """ Calculate hash of a given string in bytes
-
-        Args:
-            data: Object in bytes from which we want its hash
-
-        Returns:
-            Hash of the object
-    """
     return hashlib.sha256(data.encode(ENCODING)).hexdigest().encode(ENCODING)
 
 
 def verify_message(public_key: str, msg: str, signed_hash: bytes) -> bool:
-    """ Check if the message can be trusted
-
-        Args:
-            public_key: Path were the public key is stored
-            msg: Message
-            signed_hash: Signed hash of the message
-
-        Returns:
-            Wheter the message can be trusted or not
-    """
     msg_hash = calculate_hash(msg)
     public_key_bytes = load_public_key(public_key)
     try:
-        public_key_bytes.verify(signed_hash, msg_hash)
+        public_key_bytes.verify(
+            signed_hash,
+            msg_hash,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
     except cryptography.exceptions.InvalidSignature:
         return False
 
     return True
+
+
+def load_private_key(
+    private_key_path: str,
+    password: str = None
+) -> cryptography.hazmat.backends.openssl.rsa._RSAPrivateKey:
+    with open(private_key_path, "rb") as pem:
+        private_key = serialization.load_pem_private_key(
+            pem.read(),
+            password=password,
+            backend=default_backend()
+        )
+
+    return private_key
+
+
+def load_public_key(
+    public_key_path: str
+) -> cryptography.hazmat.backends.openssl.rsa._RSAPublicKey:
+    with open(public_key_path, "rb") as key_file:
+        pub_bytes = serialization.load_pem_public_key(
+            key_file.read(),
+            backend=default_backend()
+        )
+
+    return pub_bytes
